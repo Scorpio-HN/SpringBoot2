be82ead5dc0e77f94dfa1af22f21a4ea289150f0
Scorpio-HN <2093627508@qq.com>
2019-08-26 00:28:08 +0800

    注释掉了很多输出语句
    全部注释了

--- src/com/cssl/dao/UserController.java
+++ src/com/cssl/dao/UserController.java
@@ -3,8 +3,8 @@ package com.cssl.dao;
 public class UserController {
    //加了个注释玩玩
     public void show(){
-        System.out.println(111);
-       System.out.println(222);
-        System.out.println("2333");
+       // System.out.println(111);
+      // System.out.println(222);
+        //System.out.println("2333");
     }
 }
